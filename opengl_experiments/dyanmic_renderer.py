c71f7252d52255a618c1fd10c8c1533a93b1c15f
mjm522 <mjm522@cs.bham.ac.uk>
2017-01-27 10:05:18 +0000

    added arrow object as well

--- opengl_experiments/dyanmic_renderer.py
+++ opengl_experiments/dyanmic_renderer.py
@@ -28,16 +28,40 @@ class CubeObj():
         else:
             self._col = col
 
+class ArrowObj():
+
+    def __init__(self, pos=None, ori=None, length=None, col=None):
+
+        if pos is None:
+            self._pos = [0,0,-6.]
+        else:
+            self._pos = pos
+
+        if ori is None:
+            self._ori = [0,0,0]
+        else:
+            self._ori = ori
+
+        if length is None:
+            self._length = 2.
+        else:
+            self._length = length
+
+        if col is None:
+            self._col = [0.5,0.5,0.5, 1]
+        else:
+            self._col = col
+
 
 class DynamicRendering():
 
-    def __init__(self):
-        self._width = 640
-        self._height = 480
+    def __init__(self, width=640, height=480, posX=200, posY=200):
+        self._width   = width
+        self._height  = height
         self._objects_to_draw = []
-        self._window = 0
-        self._windowX = 200
-        self._windowY = 200    
+        self._window  = 0
+        self._windowX = posX
+        self._windowY = posY   
 
     def updateObjectList(self, new_object):
         print "Added new object!"
@@ -47,11 +71,44 @@ class DynamicRendering():
 
         #if the escape key is pressed, new object is added to the drawing list.
         if args[0] == '\033':
-            pos  = np.hstack([np.random.rand(2)*10, -6])
+            pos  = np.hstack([np.random.rand(2)*3, -6])
             ori  = np.random.rand(3)
-            size = np.random.rand(3)*2
+            size = np.random.rand(3)*6
             col  = abs(np.random.rand(3))
             self.updateObjectList(CubeObj(pos, ori, size, col))
+        elif(args[0] == 'a'):
+            pos    = np.hstack([np.random.rand(2)*4, -6])
+            ori    = np.random.rand(3)
+            length = abs(np.random.rand(1))*5
+            col    = abs(np.random.rand(3))
+            self.updateObjectList(ArrowObj(pos, ori, length, col))
+
+    def drawArrow(self, arrow):
+
+        gl.glLoadIdentity()
+
+        gl.glTranslatef(arrow._pos[0], arrow._pos[1], arrow._pos[2])
+
+        gl.glRotatef(arrow._ori[0],1.0,0.0,0.0)
+        gl.glRotatef(arrow._ori[1],0.0,1.0,0.0)
+        gl.glRotatef(arrow._ori[2],0.0,0.0,1.0)
+
+        gl.glColor3f(arrow._col[0],arrow._col[1],arrow._col[2])
+
+        # gl.glBegin(gl.GL_QUADS)
+
+        quadObj=glu.gluNewQuadric()
+        glu.gluCylinder(quadObj, 0.25, 0.25, arrow._length, 32, 16)
+
+        gl.glTranslatef(0, 0, arrow._length)
+
+        quadObj=glu.gluNewQuadric()
+        glu.gluCylinder(quadObj, 0.45, 0., 4, 32, 16)
+
+        # gl.glEnd()
+
+        arrow._ori[0] +=  - 0.30
+        arrow._ori[2] +=  - 0.30
 
     def drawCube(self, cube):
 
@@ -120,8 +177,8 @@ class DynamicRendering():
             for obj in self._objects_to_draw:
                 if isinstance(obj, CubeObj):
                     self.drawCube(obj)
-                elif obj == 'arrow':
-                    self.drawArrow()
+                elif isinstance(obj, ArrowObj):
+                    self.drawArrow(obj)
         else:
 
             print "The drawing object list empty, can't draw without a single object!"
